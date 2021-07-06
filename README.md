9795a118bfb51d1bc444d6e89dff834f243e33b1
jessejohns-1 <84858554+jessejohns-1@users.noreply.github.com>
2021-07-06 19:08:19 -0400

    mvp

--- README.md
+++ README.md
@@ -41,6 +41,22 @@ Note: Drag and drop is a bit more advanced than the others: it's not actually a 
 
 * [ ] Nest two similar events somewhere in the site and prevent the event propagation properly. Remember not all event types bubble.
 * [ ] Stop the navigation items from refreshing the page by using `preventDefault()`
+://developer.mozilla.org/en-US/docs/Web/Events) of at least 10 _different_ types. You must Use your creativity to make the Fun Bus site more interactive. For example you could change colors, animate objects, remove objects, etc. Here are some event types you could try to use:
+  * `mouseover`
+  * `keydown`
+  * `wheel`
+  * `load`
+  * `focus`
+  * `resize`
+  * `scroll`
+  * `select`
+  * `dblclick`
+  * `drag / drop`
+
+Note: Drag and drop is a bit more advanced than the others: it's not actually a single type of event but several types that need to work together.
+
+* [ ] Nest two similar events somewhere in the site and prevent the event propagation properly. Remember not all event types bubble.
+* [ ] Stop the navigation items from refreshing the page by using `preventDefault()`
 
 ### Task 3: Stretch
 
--- js/index.js
+++ js/index.js
@@ -1,1 +1,124 @@
 // Your code goes here
+window.onscroll = function () {
+    scrollRotate();
+};
+//events
+//1
+const funBus = document.querySelector(".intro img");
+//2
+const welCome = document.querySelector('.intro h2')
+
+//3
+const welComemap = document.querySelector('.content-section img')
+
+//4
+const waterWorld = document.querySelector('.inverse-content img')
+
+//5
+const funSun = document.querySelector('.content-destination img')
+//6
+const navI = document.querySelector('.nav a')
+//7
+const pickUr = document.querySelector('.content-destination h2')
+//8
+const letsG = document.querySelector('.content-section p')
+//9
+const funB = document.querySelector('.main-navigation h1')
+//10
+const footER = document.querySelector('.footer p')
+// _________________________________________________
+document.querySelector("header").style.zIndex = 999;
+
+//----------
+funBus.addEventListener("mouseover", function (event) {
+  funBus.style.borderLeft = "thick solid #0000FF";
+});
+
+funBus.addEventListener("mouseout", function (event) {
+  funBus.style.borderLeft = "none";
+});
+
+
+
+//-----------
+
+welCome.addEventListener("mouseover", function (event){
+    welCome.innerHTML = "Do you like jazz?";
+  });
+
+  welCome.addEventListener("mouseout", function (event){
+    welCome.innerHTML = "Welcome To Fun Bus!";
+  });
+
+
+//---------
+  welComemap.addEventListener("mouseover", function (event){
+    welComemap.style.transform = "rotate(20deg)";
+    
+});
+
+welComemap.addEventListener("mouseout", function (event) {
+    welComemap.style.transform = "none";
+  });
+
+ //---------
+ 
+ 
+  waterWorld.addEventListener("mouseover", function (event){
+    waterWorld.style.transform = "scaleY(1.5)";
+});
+waterWorld.addEventListener("mouseout", function (event){
+    waterWorld.style.transform = "none";
+});
+
+
+
+//---------------
+funSun.addEventListener('mouseover',function (element) {
+    document.body.style.backgroundColor = "red";
+})
+funSun.addEventListener('mouseout',function (element) {
+    document.body.style.backgroundColor = "#FFFFFF";
+})
+//-----------------------------
+
+
+navI.addEventListener('mouseover',function (element) {
+    navI.innerHTML = "It's where you make it";
+    document.body.style.backgroundColor = "blue";
+  });
+  navI.addEventListener('mouseout',function (element) {
+    navI.innerHTML = "Home";
+    document.body.style.backgroundColor = "#FFFFFF";
+
+  });
+  
+  //--------------
+
+  pickUr.addEventListener('click', function (event){
+    window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ","_blank")
+})
+//----------
+
+
+letsG.addEventListener("dblclick", function (event){
+    letsG.style.transform = "rotate(20deg)";
+    
+});
+letsG.addEventListener("mouseout", function (event){
+    letsG.style.transform = "rotate(0deg)";
+    
+});
+
+//--
+funB.addEventListener("wheel", function(event){
+    alert("YOU FOUND ME nice");
+})
+
+//-------
+
+footER.addEventListener('wheel', function(){
+    
+   alert('peanutbutter')
+    
+})
